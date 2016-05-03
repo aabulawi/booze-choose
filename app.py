138b3a25d6e670ccae962f9554b62ebab72eb44e
Ahmed Abu Lawi <ahmedlawi92@gmail.com>
2016-05-03 08:20:12 -0400

    Changes to make it faster by not making so many calls to the api

--- app.py
+++ app.py
@@ -1,14 +1,16 @@
 import os
 import bottle
-import requests
 import sqlite3
-import random
+import random_beer_selector
 
 app = bottle.Bottle()
 static_folder = os.path.join(os.path.realpath(__file__), '..', 'static')
 conn = sqlite3.connect('beers.db')
 cursor = conn.cursor()
-print static_folder
+
+
+selector = random_beer_selector.RandomBeerSelector(511)
+
 try:
     cursor.execute("CREATE TABLE BeersConsumed (BeerID int);")
 except:
@@ -24,42 +26,18 @@ def send_static(filename):
 
 @app.route('/beer-select')
 def select_beer_from_store():
-    store_id = 511
-    total_pages = get_number_of_pages(store_id)
-    order = range(1,total_pages+1)
-    random.shuffle(order)
+    if selector.should_update():
+        selector.update_beer_list()
 
     consumed_products = cursor.execute("SELECT * FROM BeersConsumed ORDER BY BeerID ASC").fetchall()
     if consumed_products:
         consumed_products = set(zip(*consumed_products)[0])
 
-    for page in order:
-        products = get_shuffled_set_of_products(page, store_id)
-        for product in products:
-            if product['id'] not in consumed_products and in_stock(store_id, product['id']):
-                cursor.execute("INSERT INTO BeersConsumed VALUES (%d)" % (product['id']))
-                conn.commit()
-                return product
-    return None
-
-def get_number_of_pages(store_id):
-    products_url = 'http://lcboapi.com/products'
-    params = {'store_id': store_id, 'per_page': 100, 'q': 'beer'}
-    return requests.get(products_url, params).json()['pager']['total_pages']
-
-def get_shuffled_set_of_products(page, store_id):
-    products_url = 'http://lcboapi.com/products'
-    params = {'store_id': store_id, 'per_page': 100, 'q': 'beer', 'page':page}
-    products = requests.get(products_url, params).json()['result']
-    random.shuffle(products)
-    return products
-
-def in_stock(store_id, product_id):
-    inventory_url = 'http://lcboapi.com/stores/%d/products/%d/inventory' % (store_id, product_id)
-    data = requests.get(inventory_url).json()
-    if data['result']['quantity'] > 0:
-        return True
-    return False
+    choice = selector.choose_random_beer(consumed_products)
+    if choice:
+        cursor.execute("INSERT INTO BeersConsumed VALUES (%d)" % (choice['id']))
+        conn.commit()
+    return choice
 
 if __name__ == '__main__':
     bottle.run(app, host='localhost', port=8000)
---
+++ random_beer_selector.py
@@ -0,0 +1,41 @@
+import datetime
+import random
+import requests
+
+
+class RandomBeerSelector():
+    def __init__(self, store_id):
+        self.last_update = datetime.datetime.min
+        self.available_beers = list()
+        self.store_id = store_id
+
+    def should_update(self):
+        return datetime.datetime.now() - self.last_update > datetime.timedelta(hours=1)
+
+    def update_beer_list(self):
+        self.last_update = datetime.datetime.now()
+        self.available_beers = list()
+        products_url = 'http://lcboapi.com/products'
+        params = {'store_id': self.store_id, 'per_page': 100, 'q': 'beer', 'page': 1}
+        beers = requests.get(products_url, params).json()
+
+        total = beers['pager']['total_pages']
+        current_page = beers['pager']['current_page']
+        while current_page <= total:
+            for beer in beers['result']:
+                if beer['inventory_count'] > 0:
+                    self.available_beers.append(beer)
+            current_page += 1
+            params['page'] = current_page
+            beers = requests.get(products_url, params).json()
+
+    def choose_random_beer(self, blacklist):
+        while self.available_beers:
+            random_choice = random.randint(0,len(self.available_beers)-1)
+            random_beer = self.available_beers[random_choice]
+            if random_beer['id'] in blacklist:
+                self.available_beers.pop(random_choice)
+            else:
+                self.available_beers.pop(random_choice)
+                return random_beer
+        return None
